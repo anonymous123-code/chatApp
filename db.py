e90325109829232e30dbf6ba3b396bbbd3f186f5
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2022-01-27 15:51:07 +0100

    Added function to change db origin

--- db.py
+++ db.py
@@ -7,6 +7,15 @@ db = json.loads(f.read())
 f.close()
 
 
+def setup(path):
+    global DB_PATH, db
+    DB_PATH = path
+    f = open(DB_PATH, "rt")
+    db = json.loads(f.read())
+    print(db)
+    f.close()
+
+
 def save():
     f = open(DB_PATH, "wt")
     f.write(json.dumps(db, indent=4))
